c26e76f1e8ba0cb920a00f83469fa0df15275fda
長門星汰 <nagatoshota@nagatohoshinoMacBook-Pro.local>
2020-09-01 15:24:42 +0900

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,4 +11,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get abut" do
+    get static_pages_about_url
+    assert_response :success
+  end
+
 end
