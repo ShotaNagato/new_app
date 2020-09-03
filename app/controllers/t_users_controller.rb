05bb88a1abc278005bb78fadf4403a70fdc59cd0
長門星汰 <nagatoshota@nagatohoshinoMacBook-Pro.local>
2020-09-03 12:41:20 +0900

    TUser sign up

--- app/controllers/t_users_controller.rb
+++ app/controllers/t_users_controller.rb
@@ -1,4 +1,24 @@
 class TUsersController < ApplicationController
-  def new
+  def show
+   @user = TUser.find(params[:id])
   end
+
+  def new
+    @user =TUser.new
+  end
+
+  def create
+    @user =TUser.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the New App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+   def user_params
+    params.require(:t_user).permit(:name, :email, :password,
+      :password_confirmation)
+   end
 end
--- app/helpers/t_users_helper.rb
+++ app/helpers/t_users_helper.rb
@@ -1,2 +1,7 @@
 module TUsersHelper
+    def gravatar_for(t_user, size:80)
+        gravatar_id = Digest::MD5::hexdigest(t_user.email.downcase)
+        gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+        image_tag(gravatar_url, alt: t_user.name, class: "gravatar")
+    end
 end
